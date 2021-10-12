3be9f661305c0d3fa7e20ad7482874de4d7b508a
Vitaliy Trach <vitaliytrach@gmail.com>
2021-10-12 08:42:59 -0700

    Added working links and a download button for my resume

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+document.getElementById("top").addEventListener("click", topClick);
+document.getElementById("iteration-zero").addEventListener("click", izClick);
+document.getElementById("spreadsheet").addEventListener("click", sClick);
+document.getElementById("business-review").addEventListener("click", brClick);
+
+function topClick() {
+    window.open("https://lemelisk27.github.io/country-holiday/", '_blank').focus();
+}
+
+function izClick() {
+    window.open("https://github.com/vitaliytrach/IterationZero", '_blank').focus();
+}
+
+function sClick() {
+    window.open("https://github.com/vitaliytrach/SchoolWork/tree/master/Spreadsheet", '_blank').focus();
+}
+
+function brClick() {
+    window.open("https://github.com/vitaliytrach/SchoolWork/tree/master/Yelp%20App", '_blank').focus();
+}
